563a59341fcfa6142c1f32a8e2a3a21333caf688
dasabhd <das.official713205@gmail.com>
2021-12-31 22:00:16 +0530

    1st Change

--- src/main/java/MainClass/Main.java
+++ src/main/java/MainClass/Main.java
@@ -1,4 +1,19 @@
 package MainClass;
 
 public class Main {
+
+     public static void main(String[] args){
+
+         System.out.println("||Welcome to Daily Expense Tracker||");
+         System.out.println("||--------------------------------||");
+         System.out.println("<<-------------------------------->>");
+
+         int choice;
+
+         System.out.println("");
+
+
+     }
+
+
 }
